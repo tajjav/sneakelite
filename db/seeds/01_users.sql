f50c0cb66d3a532fae05ce47a6a9fe560ed10255
tajjav <taj.jav@hotmail.com>
2023-11-24 23:56:01 -0700

    local stashed by tauqeer preferred

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password, is_admin ) VALUES ('Alice', 'alice@hotmail.com','password',TRUE);
+INSERT INTO users (name, email, password, is_admin ) VALUES ('John', 'john@hotmail.com','password',FALSE);
+
