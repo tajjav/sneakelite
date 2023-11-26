d14fbfb654b30ed1f207d28235449106bee3be1e
Andrew On <a_on93@hotmail.com>
2023-11-26 13:34:09 -0500

    added query to find admin and started with filtering query

---
+++ db/queries/3_admin_queries/01_find_admin.js
@@ -0,0 +1,13 @@
+const db = require('../connection');
+const findAdmin = (id) => {
+  return db
+  .query(`SELECT * from users WHERE id = $1;`, [id])
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log('error: ', err);
+  })
+}
+
+module.exports = {findAdmin};
---
+++ db/queries/4_filtering_queries/01_filtering_queries.js
@@ -0,0 +1,5 @@
+const db = require('../connection');
+const retrieveFilteredListings = function(options) {
+  const sqlParams = [];
+  let queryString = `SELECT * FROM shoe_listings`
+}
