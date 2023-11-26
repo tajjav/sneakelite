8e555c2f4d26a5146cb351db4cbbcfa99d4eaf9b
Andrew On <a_on93@hotmail.com>
2023-11-26 12:56:04 -0500

    added in favourites routes and cookie functionality

---
+++ routes/favourites-api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router  = express.Router();
+const {favouritesListing, showUserFavourites} = require('../db/queries/2_favourites_queries/01_favourites_queries');
+
+//shoeId is the variable to be used in client scripts at public/scripts/app.js (can be changed later)
+router.post('/', (req, res) => {
+  favouritesListing(req.body.shoeId, req.session.user_id.id)
+  .then((shoeListing) => {
+    console.log('showListingId: ', req.body.shoeId);
+    return res.json(shoeListing);
+  })
+})
+
+router.get('/', (req, res) => {
+  let templateVars = {};
+  showUserFavourites(req.session.user_id.id)
+  .then((result) => {
+    templateVars['favourites'] = result;
+    res.render('favourites', templateVars);
+  })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+const cookieSession = require('cookie-session');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
