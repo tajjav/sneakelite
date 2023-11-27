bbbdac5d5116bc1e5e98b8d586a129626fcb26d3
tajjav <taj.jav@hotmail.com>
2023-11-27 12:40:42 -0700

    add cookie-session, login logout functionality using name, modify index.ejs

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 
-module.exports = () => {
-  router.get('/login', (req,res) => {
+  // Auth login
+  router.post('/login', (req,res) => {
+    const {userName} = req.body;
+    console.log('userName: ', userName);
+    req.session.name = userName;
+    res.redirect('/');
+  });
+
+
+  // Login rendering
+  router.get('/login/:id', (req,res) => {
     req.session.user_id = req.params.id;
-  })
-}
+    res.redirect('/');
+  });
+
+  // Auth logout
+  router.post('/logout', (req,res) => {
+    req.session = null;
+    res.redirect('/');
+  });
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -5,11 +5,15 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
-const session = require('express-session');
+const cookieSession = require('cookie-session');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
-
+app.use(cookieSession({
+  name: "session",
+  keys: ["OulAla", "seEhaa", "croPtop"],
+  maxAge: 24 * 60 * 60 * 1000
+}));
 app.set('view engine', 'ejs');
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
@@ -17,6 +21,7 @@ app.set('view engine', 'ejs');
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   '/styles',
   sassMiddleware({
@@ -26,6 +31,7 @@ app.use(
   })
 );
 app.use(express.static('public'));
+
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -38,6 +44,8 @@ const listingsApiRoutes = require('./routes/listings-api');
 // const favouritesRoutes = require('./routes/favourites');
 // const messagesApiRoutes = require('./routes/messages-api');
 // const messagesRoutes = require('./routes/messages');
+const loginRoutes = require('./routes/login');
+const db = require('./db/connection');
 
 
 
@@ -56,6 +64,7 @@ app.use('/api/listings', listingsApiRoutes);
 
 app.use('/api/users', userApiRoutes);
 app.use('/users', usersRoutes);
+app.use('/', loginRoutes);
 
 app.use('/api/widgets', widgetApiRoutes);
 // Note: mount other resources here, using the same pattern above
@@ -65,7 +74,20 @@ app.use('/api/widgets', widgetApiRoutes);
 // Separate them into separate routes files (see above).
 
 app.get('/', (req, res) => {
-  res.render('index');
+  let userName = req.session.name;
+  
+  if(!userName) {
+    res.render('index', {userName});
+  } else {
+    db.query("SELECT * FROM users WHERE name = $1", [userName])
+      .then((data) => {
+        userName = data.rows[0].name;
+        res.render("index", {userName});  
+      })
+      .catch((error) => {
+        return console.error(error);
+      });
+  }
 });
 
 app.listen(PORT, () => {
